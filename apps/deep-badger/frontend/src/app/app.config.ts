aa201eee008e5de141544b2ad446fc2e6d8e3cbc
w.alexander <will_alexander@me.com>
2025-12-04 23:23:16 +0100

    Add PrimeNG

--- apps/deep-badger/frontend/src/app/app.config.ts
+++ apps/deep-badger/frontend/src/app/app.config.ts
@@ -1,10 +1,17 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(appRoutes),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ],
 };
